de54f7c51c0a393122da54fd82a39851472dcaa6
Genesis Macero <Genesis Macero@DESKTOP-SH0RTD0>
2025-04-03 15:27:01 -0500

    Agregar archivos de proyecto.

---
+++ FigurasGeometricas/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Figuras_Geometricas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbFigura = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtContador = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbFigura
+            // 
+            this.cmbFigura.FormattingEnabled = true;
+            this.cmbFigura.Items.AddRange(new object[] {
+            "Rectangulo",
+            "Triangulo",
+            "Circulo",
+            "Linea"});
+            this.cmbFigura.Location = new System.Drawing.Point(98, 90);
+            this.cmbFigura.Name = "cmbFigura";
+            this.cmbFigura.Size = new System.Drawing.Size(121, 21);
+            this.cmbFigura.TabIndex = 0;
+            this.cmbFigura.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Elegir Figura ";
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(98, 149);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(121, 20);
+            this.txtX.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Cordenada en x";
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(98, 184);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(121, 20);
+            this.txtY.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Cordenada en y";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Contador";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, 377);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 37);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Crear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.pictureBox1.Location = new System.Drawing.Point(80, 292);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(118, 79);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel1.Location = new System.Drawing.Point(247, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(541, 426);
+            this.panel1.TabIndex = 10;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 305);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Elegir color";
+            // 
+            // txtContador
+            // 
+            this.txtContador.Location = new System.Drawing.Point(98, 240);
+            this.txtContador.Name = "txtContador";
+            this.txtContador.ReadOnly = true;
+            this.txtContador.Size = new System.Drawing.Size(121, 20);
+            this.txtContador.TabIndex = 12;
+            this.txtContador.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtContador);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbFigura);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbFigura;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtContador;
+    }
+}
+
---
+++ FigurasGeometricas/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Figuras_Geometricas
+{
+    public partial class Form1 : Form
+    {
+        private List<Figura> figuras = new List<Figura> (); 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Leer y validar coordenadas
+                int x = int.Parse(txtX.Text);
+                int y = int.Parse(txtY.Text);
+
+                // Obtener el tipo de figura seleccionada
+                string tipoFigura = cmbFigura.SelectedItem.ToString();
+
+                // Crear figura usando la Factory
+                Figura figura = FiguraFactory.CrearFigura(tipoFigura, pictureBox1.BackColor, x, y);
+
+                figuras.Add(figura);
+
+                panel1.Invalidate();
+
+                // Incrementar contador y actualizar el TextBox
+                /*   int numero = int.Parse(txtContador.Text) + 1;
+                   txtContador.Text = txtContador.ToString(); */
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Entrada inválida", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            using (ColorDialog colorDialog = new ColorDialog())
+            {
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.BackColor = colorDialog.Color;
+                }
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            // Dibujar todas las figuras almacenadas en la lista
+            foreach (Figura figura in figuras)
+            {
+                figura.Dibujar(g);
+            }
+        }
+    }
+}
