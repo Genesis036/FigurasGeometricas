5beeb69647f686b181dfc2fa34100310ec9c67af
Genesis Macero <Genesis Macero@DESKTOP-SH0RTD0>
2025-04-03 15:37:09 -0500

    Se creo la clase abstractra figura

---
+++ FigurasGeometricas/Figura.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figuras_Geometricas
+{
+    using System.Drawing;
+
+    public abstract class Figura
+    {
+        public Color Color { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        protected Figura(Color color, int x, int y)
+        {
+            Color = color;
+            X = x;
+            Y = y;
+        }
+
+        public abstract void Dibujar(Graphics g);
+    }
+
+}
